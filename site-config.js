83b601f729ad89967694f12ff9b27c7d28e7a3c5
leungwensen <leungwensen@gmail.com>
2018-09-19 17:28:34 +0800

    chore: add g6 demos back to screenshots

--- site-config.js
+++ site-config.js
@@ -53,6 +53,11 @@ module.exports = {
             dest: '${assets}/dist/${pkg.version}/f2/3.x',
             template: 'f2-demo-standalone'
         },
+        {
+            src: 'zh-cn/g6/1.x/demo',
+            dest: '${assets}/dist/${pkg.version}/g6/1.x',
+            template: 'g6-demo-standalone'
+        },
     ],
     indices: [{
         src: 'zh-cn',
