be5ef1fe2cec354acf0d8020c1ab0293b311b9f7
Elaine1234 <yilin.qyl@antfin.com>
2018-10-24 21:06:50 +0800

    feat(demos): hide category if no match was found

--- site/zh-cn/g2/3.x/demo/data.js
+++ site/zh-cn/g2/3.x/demo/data.js
@@ -59,6 +59,7 @@ demoDirs.forEach(function (dir) {
         var index = meta.index,
             title = meta.title,
             recommend = meta.recommend,
+            filter = meta.filter,
             tags = meta.tags;
         var name = basename(file, '.html');
         var href = base + 'zh-cn/g2/3.x/demo/' + category + '/' + name + '.html';
@@ -71,7 +72,7 @@ demoDirs.forEach(function (dir) {
             category: category,
             title: title,
             recommend: recommend,
-            tag: tags,
+            tag: filter,
             tags: tags ? tags.split(',') : null
         };
         demoByHref[href] = demo;
--- theme/default/src/demos.js
+++ theme/default/src/demos.js
@@ -18,101 +18,153 @@ function showRecommend($card) {
       $card.show();
     } else {
       $card.hide();
+      return false;
     }
   } else {
     $card.show();
   }
+  return true;
+}
+
+function filterCategory(cards, keywords) {
+  let flag = false, hasMatch = false, $card, tags;
+  cards.each(function () {
+    $card = $(this);
+    if (keywords.length == 0) {
+      hasMatch = showRecommend($card) || hasMatch;
+    } else {
+      tags = $card.attr('data-tags') === '' ? null : $card.attr('data-tags').split(',');
+      if (!tags) {
+        $card.hide();
+      } else {
+        for (var i = 0; i < tags.length; i++) {
+          for (var j = 0; j < keywords.length; j++) {
+            if (tags[i] === keywords[j]) {
+              flag = true;
+              break;
+            }
+          }
+          if (flag) {
+            break;
+          }
+        }
+        if (flag) {
+          hasMatch = showRecommend($card) || hasMatch;
+        } else {
+          $card.hide();
+        }
+        flag = false;
+      }
+    }
+  });
+  return hasMatch;
 }
 
 function filterDemos(keywords) {
-  let $card, tags, flag = false;
- $('.card-container').each(function() {
-   $card = $(this);
-   if (keywords.length == 0) {
-     showRecommend($card);
-     return;
-   }
-   tags = $card.attr('data-tags') === '' ? null : $card.attr('data-tags').split(',');
-   if (!tags) {
-     $card.hide();
-     return;
-   }
-   for (var i = 0; i < tags.length; i++) {
-     for (var j = 0; j < keywords.length; j++) {
-       if(tags[i] === keywords[j]) {
-         flag = true;
-         break;
-       }
-     }
-     if (flag) {
-       break;
-     }
-   }
-   if (flag) {
-    showRecommend($card);
-   } else {
-     $card.hide();
-     }
-   flag = false;
-   });
+  let $card, tags, flag = false, hasMatch = false;
+  $('h5').each(function () {
+    const $cat = $(this);
+    const cards = $cat.next('.row').find('.card-container');
+    flag = filterCategory(cards, keywords);
+    hasMatch = flag || hasMatch;
+    if (!flag) {
+      $cat.hide();
+    } else {
+      $cat.show();
+    }
+  });
+  $('.card-container').each(function () {
+    $card = $(this);
+    if (keywords.length == 0) {
+      hasMatch = showRecommend($card) || hasMatch;
+    } else {
+      tags = $card.attr('data-tags') === '' ? null : $card.attr('data-tags').split(',');
+      if (!tags) {
+        $card.hide();
+      } else {
+        for (var i = 0; i < tags.length; i++) {
+          for (var j = 0; j < keywords.length; j++) {
+            if (tags[i] === keywords[j]) {
+              flag = true;
+              break;
+            }
+          }
+          if (flag) {
+            break;
+          }
+        }
+        if (flag) {
+          hasMatch = showRecommend($card) || hasMatch;
+        } else {
+          $card.hide();
+        }
+        flag = false;
+      }
+    }
+  });
+  if (!hasMatch) {
+    $('#noResult').show();
+  } else {
+    $('#noResult').hide();
+  }
 }
 
 $('.theme-switching .btn').each(function () {
-    const $btn = $(this);
-    if (isDark) {
-        if ($btn.data('theme') === 'dark') {
-            $btn.removeClass('btn-light').addClass('btn-primary');
-            $('a.screenshot-container').each(function () {
-                const $link = $(this);
-                const href = $link.attr('href');
-                $link.attr('href', `${href}?theme=dark`);
-            });
-        } else {
-            $btn.removeClass('btn-primary').addClass('btn-light');
-            $('a.screenshot-container').each(function () {
-                const $link = $(this);
-                const href = $link.attr('href');
-                $link.attr('href', href.replace('?theme=dark', ''));
-            });
-        }
+  const $btn = $(this);
+  if (isDark) {
+    if ($btn.data('theme') === 'dark') {
+      $btn.removeClass('btn-light').addClass('btn-primary');
+      $('a.screenshot-container').each(function () {
+        const $link = $(this);
+        const href = $link.attr('href');
+        $link.attr('href', `${href}?theme=dark`);
+      });
+    } else {
+      $btn.removeClass('btn-primary').addClass('btn-light');
+      $('a.screenshot-container').each(function () {
+        const $link = $(this);
+        const href = $link.attr('href');
+        $link.attr('href', href.replace('?theme=dark', ''));
+      });
     }
+  }
 });
 
 tocbot.init({
-    tocSelector: '.toc',
-    contentSelector: '.demos .content',
-    headingSelector: 'h1, h2, h3, h4, h5, h6',
-    collapseDepth: 4
+  tocSelector: '.toc',
+  contentSelector: '.demos .content',
+  headingSelector: 'h1, h2, h3, h4, h5, h6',
+  collapseDepth: 4
 });
 
 const $tocContainer = $('.toc-container');
 $('header').headroom({
-    offset: 0,
-    tolerance: 0,
-    classes: {
-        initial: 'animated',
-        pinned: 'slideDown',
-        unpinned: 'slideUp'
-    },
-    onPin() {
-        $tocContainer.css({
-            height: 'calc(100% - 120px)',
-        });
-    },
-    onUnpin() {
-        $tocContainer.css({
-            height: 'calc(100% - 48px)',
-        });
-    }
+  offset: 0,
+  tolerance: 0,
+  classes: {
+    initial: 'animated',
+    pinned: 'slideDown',
+    unpinned: 'slideUp'
+  },
+  onPin() {
+    $tocContainer.css({
+      height: 'calc(100% - 120px)',
+    });
+  },
+  onUnpin() {
+    $tocContainer.css({
+      height: 'calc(100% - 48px)',
+    });
+  }
 });
 
-$('.tag').on('click', function(e) {
+$('.tag').on('click', function (e) {
   const tag = $(e.target);
   const data = tag.attr('data-text');
-  if(data === RECOMMEND) {
+  if (data === RECOMMEND) {
     recommendOnly = !recommendOnly;
   }
-  if(tag.hasClass('selected')) {
+  if (tag.hasClass('selected')) {
     tag.removeClass('selected');
     if (data !== RECOMMEND) {
       delete selectedTags[data];
@@ -126,7 +178,7 @@ $('.tag').on('click', function(e) {
   filterDemos(Object.keys(selectedTags));
 });
 
-$('#toggleSearch').click(function() {
+$('#toggleSearch').click(function () {
   if (expansion) {
     $('#searchContainer').addClass('mini');
     $('#toggleSearch').text(SHOW_SEARCH);
@@ -140,37 +192,37 @@ $('#toggleSearch').click(function() {
 });
 
 $('.screenshot').each(function () {
-    const $img = $(this);
-    if (isDark) {
-        $('.demos').addClass('dark');
-        $img.attr('data-src', $img.data('dark'));
-        $img.attr('src', `${__meta.assets}/image/screenshot-placeholder-dark.png`);
-    } else {
-        $img.attr('data-src', $img.data('default'));
-        $img.attr('src', `${__meta.assets}/image/screenshot-placeholder.png`);
-    }
+  const $img = $(this);
+  if (isDark) {
+    $('.demos').addClass('dark');
+    $img.attr('data-src', $img.data('dark'));
+    $img.attr('src', `${__meta.assets}/image/screenshot-placeholder-dark.png`);
+  } else {
+    $img.attr('data-src', $img.data('default'));
+    $img.attr('src', `${__meta.assets}/image/screenshot-placeholder.png`);
+  }
 });
 
 $('.lazyload').lazyload();
 
 function adjustScreenshotsSize() {
-    $('.screenshot').each(function () {
-        const $screenshot = $(this);
-        if (!$('.demos').hasClass('f2')) {
-          if ($screenshot.attr('data-recommend') === 'true') {
-            $screenshot.height($screenshot.width() / 16 * 9);
-          } else {
-            $screenshot.height($screenshot.width() / 16 * 9 + 38);
-          }
-        } else {
-          $screenshot.height($screenshot.width() * 0.535);
-        }
-    });
+  $('.screenshot').each(function () {
+    const $screenshot = $(this);
+    if (!$('.demos').hasClass('f2')) {
+      if ($screenshot.attr('data-recommend') === 'true') {
+        $screenshot.height($screenshot.width() / 16 * 9);
+      } else {
+        $screenshot.height($screenshot.width() / 16 * 9 + 38);
+      }
+    } else {
+      $screenshot.height($screenshot.width() * 0.535);
+    }
+  });
   $tocContainer.height($('.demos').height());
 }
 
 adjustScreenshotsSize();
-$('.toc-link').each(function() {
+$('.toc-link').each(function () {
   const $cat = $(this);
   const style = 'iconfont ' + $('#_' + $cat.text()).attr('data-icon');
   $cat.append('<span class="' + style + '"/>');
