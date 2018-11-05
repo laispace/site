1871cd453819d5d3eccf8052535e4459e6c53148
Elaine1234 <yilin.qyl@antfin.com>
2018-11-05 20:40:33 +0800

    fix(demos): mdify icon for bar charts

--- site/zh-cn/g2/3.x/data.js
+++ site/zh-cn/g2/3.x/data.js
@@ -1,6 +1,6 @@
 const plotByName = {
     area:        { icon: 'icon-area', index: 4,  url: '${base}zh-cn/g2/3.x/demo/area/basic.html', name: '面积图', },
-    column:      { icon: 'icon-bar', index: 1,  url: '${base}zh-cn/g2/3.x/demo/column/basic.html', name: '柱状图', },
+    column:      { icon: 'icon-column', index: 1,  url: '${base}zh-cn/g2/3.x/demo/column/basic.html', name: '柱状图', },
     bar:         { icon: 'icon-bar', index: 1,  url: '${base}zh-cn/g2/3.x/demo/bar/basic.html', name: '条形图', },
     box:         { icon: 'icon-box', index: 5,  url: '${base}zh-cn/g2/3.x/demo/box/basic.html', name: '箱形图', },
     component:   { icon: 'icon-component', index: 0,  url: '${base}zh-cn/g2/3.x/demo/component/legend-position.html', name: '组件使用', },
