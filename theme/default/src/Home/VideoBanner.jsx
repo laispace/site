b606ff70853422c94a51d0040252859031ee43ea
Elaine1234 <yilin.qyl@antfin.com>
2018-11-12 21:06:23 +0800

    feat(index): add overlay on top of video

--- theme/default/src/Home/VideoBanner.jsx
+++ theme/default/src/Home/VideoBanner.jsx
@@ -30,6 +30,7 @@ class Banner extends React.Component {
 
   render() {
     return (
+      <div>
       <section id="video-container" style={{ height: this.state.height }}>
         <video
           autoplay="autoplay"
@@ -37,18 +38,22 @@ class Banner extends React.Component {
           muted="muted"
           controls={false}
           style={{ width: '100%', height: '100%', objectFit: 'fill' }}
-          src="https://ur.alipay.com/rmsportal_560_test.mov"></video>
-        <div className="homepage-title">
-          <div style={{fontSize: 52, fontWeight: 'bolder'}}>让数据栩栩如生</div>
-          <p style={{marginTop: 20, color: 'rgba(255,255,255,.85)'}}>
-            AntV 是蚂蚁金服全新一代数据可视化解决方案，致力于提供一套简单<br />方便、专业可靠、无限可能的数据可视化最佳实践。
-          </p>
-          <a href="#__products">
-            <button className="btn-more">继续了解</button>
-          </a>
+          src="https://ur.alipay.com/rmsportal_560_test.mov">
+        </video>
+        <div className="overlay">
+          <div className="homepage-title">
+            <div style={{fontSize: 52, fontWeight: 'bolder'}}>让数据栩栩如生</div>
+            <p style={{marginTop: 20, color: 'rgba(255,255,255,.85)'}}>
+              AntV 是蚂蚁金服全新一代数据可视化解决方案，致力于提供一套简单<br />方便、专业可靠、无限可能的数据可视化最佳实践。
+            </p>
+            <a href="#__products">
+              <button className="btn-more">继续了解</button>
+            </a>
+          </div>
+          <img id="arrow" src="https://gw.alipayobjects.com/zos/rmsportal/cfUeahyjXCvoeUHrZDcE.svg" width="64" height="64" />
         </div>
-        <img id="arrow" src="https://gw.alipayobjects.com/zos/rmsportal/cfUeahyjXCvoeUHrZDcE.svg" width="64" height="64" />
       </section>
+      </div>
     );
   }
 }
