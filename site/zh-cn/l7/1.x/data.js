3f4f3db0d242e31369b5b75e47554eb333b3db8d
李正学 <zhengxue.lzx@antfin.com>
2018-11-20 20:33:13 +0800

    fix(icon):icon

--- site/zh-cn/l7/1.x/data.js
+++ site/zh-cn/l7/1.x/data.js
@@ -1,8 +1,8 @@
 const plotByName = {
-    point:        { icon: 'icon-area', index: 0,  url: '${base}zh-cn/l7/1.x/demo/point/pointcircle.html', name: '点图层', },
+    point:        { icon: 'icon-dain', index: 0,  url: '${base}zh-cn/l7/1.x/demo/point/pointcircle.html', name: '点图层', },
     line:      { icon: 'icon-xian', index: 1,  url: '${base}zh-cn/l7/1.x/demo/line/arcline.html', name: '线图层', },
-    polygon:         { icon: 'icon-bar', index: 2,  url: '${base}zh-cn/l7/1.x/demo/polygon/choropleths.html', name: '面图层', },
-    raster:   { icon: 'icon-component', index: 4,  url: '${base}zh-cn/l7/1.x/demo/raster/dem.html', name: '栅格图层', },
+    polygon:         { icon: 'icon-mian', index: 2,  url: '${base}zh-cn/l7/1.x/demo/polygon/choropleths.html', name: '面图层', },
+    raster:   { icon: 'icon-pic', index: 4,  url: '${base}zh-cn/l7/1.x/demo/raster/dem.html', name: '栅格图层', },
 
 };
 module.exports = {
